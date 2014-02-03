fb193fa61872d8f7f956d476aa7feff9862b2322
Max Coder <mckenn@gmail.com>
2014-02-03 15:05:20 +0200

    add seed for categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.create title: "Эвакуатор", logo: File.open(File.join(Rails.root, 'app/assets/images/services/evacuator.png'))
+Category.create title: "Открывание дверей", logo: File.open(File.join(Rails.root, 'app/assets/images/services/door-open.png'))
+Category.create title: "Ремонт шин", logo: File.open(File.join(Rails.root, 'app/assets/images/services/tyre.png'))
+Category.create title: "Юрист", logo: File.open(File.join(Rails.root, 'app/assets/images/services/lawyer.png'))
+Category.create title: "Аварийный комиссар", logo: File.open(File.join(Rails.root, 'app/assets/images/services/insurance.png'))
+Category.create title: "Авто-страхование", logo: File.open(File.join(Rails.root, 'app/assets/images/services/tyre.png'))
+Category.create title: "Аккум-сервис", logo: File.open(File.join(Rails.root, 'app/assets/images/services/acum-service.png'))
+Category.create title: "Авторемонт", logo: File.open(File.join(Rails.root, 'app/assets/images/services/auto-repair.png'))
+Category.create title: "ГАИ", logo: File.open(File.join(Rails.root, 'app/assets/images/services/gai.png'))
