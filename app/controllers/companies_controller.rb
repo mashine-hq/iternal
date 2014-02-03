7dfec370b4879799c7dccd0cff796f75597665b8
Max Coder <mckenn@gmail.com>
2014-02-03 22:41:37 +0200

    add home lauout

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,6 @@
 class CompaniesController < ApplicationController
   before_action :set_company, only: [:show, :edit, :update, :destroy]
-
+  layout "home"
   # GET /companies
   # GET /companies.json
   def index
