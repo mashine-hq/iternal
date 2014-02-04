dd2ee44e03c225826cdf9e7e567cf99efa41c3fb
Max Coder <mckenn@gmail.com>
2014-02-04 16:46:51 +0200

    setup capistrano for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,10 +23,10 @@ set :deploy_to, '/home/deploy/apps/mobiler/'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/application.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system /public/uploads}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
